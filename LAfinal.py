68ec9a819d16a66b24df3f0c8466cc70c5e0b9a3
Ezekiel <90792131+Zeke9215@users.noreply.github.com>
2021-11-05 19:56:05 -0500

    Add files via upload

--- LAfinal.py
+++ LAfinal.py
@@ -211,7 +211,7 @@ print(matrix_b1[1][0])
 print(matrix_matrix_multi(matrix_a1,matrix_b1))
 
 
-#####################################################################################################################
+#######################################################################################homework04
 
 #1 absval
 
@@ -305,50 +305,16 @@ def p_norm(vector: list, scalar: float ) ->float:
 
 
 
-#####################HW5#####################################
+#####################HW5#####################################Homework05
 
 
 
-#1
+#1 Deleted Unstable QR
 
 matrix_qr1 = [[1,1,1],[-1,1,0],[1,2,1]]
 matrix_qr2 = [[1,1,3],[2,2,2],[2,2,-1]]
 
-def unstable_QR(matrix:list) -> list:
-    """A function that performs the unstable Gram-Schmidt to return the QR Factorization of a given matrix.
-Args: A matrix stored as a list of lists.
-Returns: The QR factorization as a new matrix"""
-    Q = [0 for elements in matrix]
-    v = [0 for elements in matrix]
-    r = [[0] * len(matrix[0]) for elements in matrix]
-    for j in range (len(matrix)):
-        v[j] = matrix[j]
-        for k in range(j):
-            r[j][k] = inner_product(Q[k],v[j])
-            v[j] = add_vectors(v[j],scalar_vector_multi(-r[j][k],Q[k]))
-        r[j][j] = p_norm(v[j],2)
-        Q[j] = scalar_vector_multi(1/r[j][j],v[j])
-    return [Q,r]
 
-print("problem1 unstableQR####################################")
-print(unstable_QR(matrix_qr1))
-
-print("""answer for matrix_qr1 should be [[[0.5773502691896258, 0.5773502691896258, 0.5773502691896258],
-  [-0.7071067811865475, 0.7071067811865475, 0.0],
-  [0.4082482904638618, 0.40824829046386235, -0.8164965809277268]],
- [[1.7320508075688772, 0, 0],
-  [0.0, 1.4142135623730951, 0],
-  [2.3094010767585034, 0.7071067811865475, 0.408248290463863]]]""")
-
-print("input2-> matrix_qr2")
-print(unstable_QR(matrix_qr2))
-
-print("""answer for matrix_qr2 should be [[[0.30151134457776363, 0.30151134457776363, 0.9045340337332909],
-  [0.6396021490668313, 0.6396021490668313, -0.42640143271122105],
-  [0.0, 0.0, 1.0]],
- [[3.3166247903554, 0, 0],
-  [3.0151134457776365, 1.7056057308448835, 0],
-  [0.30151134457776363, 2.984810028978546, 6.661338147750939e-16]]]""")
 
 
 
@@ -394,3 +360,41 @@ print("""answer for matrix_qr2 should be [[[0.30151134457776363, 0.3015113445777
 
 
 
+#####################################Homework06
+
+#1 Delete unstable gs
+
+
+#2 Orthonomal list return
+
+def orthonormal_list_return(matrix:list) -> list:
+    """A Function that returns an orthonormal list of vectors from a given list of vectors.
+    Uses the Qr factorization but only returns Q.
+        Args: A matrix stored as a list of lists.
+        Returns: The list of orthonormal vectors as a new matrix"""
+    Q = [[0] * len(matrix[0]) for elements in matrix]
+    v = [0 for elements in matrix]
+    r = [[0] * len(matrix[0]) for elements in matrix]
+    for j in range(len(matrix)):
+        v[j] = matrix[j]
+    for k in range(len(matrix)):
+        r[k][k] = p_norm(v[k], 2)
+        Q[k] = scalar_vector_multi(1 / r[k][k], v[k])
+        for j in range(len(matrix)):
+            r[j][k] = inner_product(Q[k], v[j])
+            v[j] = add_vectors(v[j], scalar_vector_multi(-r[j][k], Q[k]))
+
+    return [Q]
+
+print(orthonormal_list_return(matrix_qr1))
+
+print("""answer for matrix_qr1 should be [[[0.5773502691896258, 0.5773502691896258, 0.5773502691896258],
+ [-0.7071067811865475, 0.7071067811865475, 0.0],
+  [0.4082482904638618, 0.40824829046386235, -0.8164965809277268]]""")
+
+print("input 2 -> matrix_qr2")
+print(orthonormal_list_return(matrix_qr2))
+
+print("""answer for matrix_qr2 should be [[[0.30151134457776363, 0.30151134457776363, 0.9045340337332909],
+  [0.6396021490668313, 0.6396021490668313, -0.42640143271122105],
+  [0.0, 0.0, 1.0]]""")
