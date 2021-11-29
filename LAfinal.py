05db8bd065c3576838040fc54e21c2bfa0169d28
Ezekiel <90792131+Zeke9215@users.noreply.github.com>
2021-11-28 23:52:31 -0600

    Update LAfinal.py

--- LAfinal.py
+++ LAfinal.py
@@ -401,8 +401,7 @@ print("""answer for matrix_qr2 should be [[[0.30151134457776363, 0.3015113445777
 
 
 
-
-#############################################################################################################################
+######HOUSEHOLDER
 
 
 
@@ -424,7 +423,10 @@ def reflect_vector(vector_1: list)->list:
     output: the reflected vector as a list v"""
     e = [0 for element in range(len(vector_1))]
     e[0] = 1
-    v = add_vectors(scalar_vector_multi(sign(vector_1[0])  * p_norm(vector_1,e), vector_1))
+    addend = scalar_vector_multi(sign(vector_1[0])  * boolean_p_norm(vector_1),e)
+
+    v = add_vectors(addend, vector_1)
+
     return v
 
 
@@ -440,7 +442,7 @@ def identity(size: int)->int:
 
 
 def deep_copy(matrix_1: list) -> list:
-    "this funcrion takes Q from Stable Grahm schmidt"
+    "this function takes Q from Stable Grahm schmidt"
     empty: list = [[0 for element in range(len(matrix_1[0]))] for index in range(len(matrix_1))]
     for x in range(len(matrix_1[0])):
         for y in range(len(matrix_1[0])):
@@ -474,24 +476,26 @@ def v_v_multi(vector_1,vector_2):
 
 def f_builder(vector_1: list) -> list:
     """This function returns the F component of the Householder algorithm """
-    s = -2/(p_norm(vector_1))**2
-    x = scalar_vector_multi(s,v_v_multi(vector_1,vector_1))
+    s = -2/(boolean_p_norm(vector_1))**2
+    x = scalar_matrix_multi(s,v_v_multi(vector_1,vector_1))
     y = matrix_matrix_add(identity(len(vector_1)), x)
     return y
 
 
 def Q_build(mtx :list, n: int):
     """This function builds Q for the Householder"""
-    A:list = [[0 for j in range(n,len(mtx[i]))for i in range(n,len(mtx))]]
+    A: list = [[0 for j in range (n, len(mtx[i]))]for i in range(n,len(mtx))]
     for i in range(len(mtx)):
         for j in range(len(mtx[i])):
-            if n+j < len(mtx[i]):
-                A[i][j] = mtx[n+i][n+j]
+            if n+i < len(mtx[i]):
+                if n+j < len(mtx[i]):
+                    A[i][j] = mtx[n+i][n+j]
     v = reflect_vector(A[0])
     f = f_builder(v)
     Q = identity(len(mtx))
     for i in range(n,len(Q)):
-        Q[i][j] = f[i-n][j-r]
+        for j in range(n, len(Q)):
+            Q[i][j] = f[i-n][j-n]
     return Q
 
 
@@ -506,27 +510,83 @@ def Householder(matrix_A: list) ->list:
     R: list = deep_copy(matrix_A)
     Q_list: list = []
     for index in range(len(R)):
-        Q_temp: list = Q_build(R)
+        Q_temp: list = Q_build(R,index)
         R = matrix_matrix_multi(Q_temp, R)
         Q_list.append(Q_temp)
     Q: list = Q_list[-1]
     Q: list = conjugate_transpose(Q_list[0])
-    for index in range(1, len(Q_list)):
-        Q = matrix_matrix_multi(Q, conjugate_transpose(Q_list[index]))
+    for index1 in range(1, len(Q_list)):
+        ct = conjugate_transpose(Q_list[index1])
+        Q = matrix_matrix_multi(Q, ct)
     return Q, R
 
 
-#print(Householder([[2,2,1],[-2,1,2],[18,0,0]]))
+
+
+
+print(Householder([[2,2,1],[-2,1,2],[18,0,0]]))
 
 #print(sign(-3))
-print(reflect_vector([2,2,1]))
-print(identity(4))
+#print(reflect_vector([2,2,1]))
+#print(identity(1))
+#print(deep_copy([[2,2],[1,1]]))
+#print(conjugate_transpose([[2,3],[5,2-3j]]))
+#print(v_v_multi([5,2,1],[5,2,1]))
+#print(f_builder([4.8, 2.4]))
+#print(Q_build(f_builder([4.8, 2.4]), 1))
 
-#print(f_builder([[2,3,4],[1,2,3],[1,1,1]]))
-#print(Q_build(
-#print(Householder
 
 
+
+def backsub(matrix_a: list, vector_a:list) -> list:
+    """This Function solves the upper triangular matrix and an input vector to return the solution vector, where
+     Ax=B A is the upper triangular matrix and b is the input vector. x is the solution vector returned."""
+
+    """Starting from the last column, take the diagonal and subtract the elements to the to the right multiplied by the solution
+     and then proceed to divide by the diagonal."""
+
+    """ARGS:
+    matrix_a: A list of a list that represents the upper triangular matrix
+    vector_a: An input vector stored as a list. """
+
+    """Returns:
+    Returns the solution vector."""
+
+    result: list = [vector_a[-1]*(1/(matrix_a[-1][-1]))]
+    for element in range(len(matrix_a) - 2, -1, -1):
+        scal: float = vector_a[element]
+        for index in range(len(result)):
+            scal -=matrix_a[len(matrix_a)-1-index][element]*result[index]
+        scal *=1/(matrix_a[element][element])
+        result.append(scal)
+    return result[: : -1]
+
+
+
+def least_squares(matrix_a: list, vector_a: list) -> list:
+    """Creates the least squares matrix from the input matrix an d vector."""
+
+    """Least squares via QR factorization
+    First we use householder to get the reduced QR factorization. Next we use the conjugate transpose function on Q 
+    and set to a variable called Q_1. this is Q*b. Then we solve the Triangular system by multiplying the conjugate
+     transpose of Q by vector A and calling it Q_2 Finally set the result to backsub(R,Q_2).
+    """
+    """ARGS:
+    matrix_a: A matrix stored as a list of lists
+    vector_a: a vector stored as a list"""
+
+    """Returns:
+    The least squares matrix"""
+
+
+    Q, R = Householder(matrix_a)
+    Q_1 = conjugate_transpose(Q)
+    Q_2 = matrix_vector_multi(Q_1,vector_a)
+    result = backsub(R, Q_2)
+    return result
+
+print("LS")
+print(least_squares([[1,2,3],[1,1,2],[2,1,2]], [3,3,1]))
 
 
 
